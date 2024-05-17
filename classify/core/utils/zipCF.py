a35f7dc03aa1f5ab423aa93867defa7524de5d89
Ernist Seidiakmatov <ernistseidiakmatov28@gmail.com>
2024-05-17 22:48:31 +0900

    right file path

--- classify/core/utils/zipCF.py
+++ classify/core/utils/zipCF.py
@@ -21,7 +21,7 @@ class ZipClassifier:
 
     @classmethod
     def classify(cls, input_dir, output_dir):
-        model = YOLO('classify\core\utils\zipCF.py')
+        model = YOLO('classify\\core\\utils\\best3.pt')
         classes = ["balaclava", "banknote", "baseball_bat", "blood",
             "cigarette", "drug", "fire", "knife", "pistol", "rifle", "other",]
         if not os.path.exists(output_dir):
